723fed855416d3cb8480fde80f69de1d5fbb0896
M-Saffan75 <saffan.khan175@gmail.com>
2024-01-14 20:35:19 -0700

    first commit

--- src/provider/Pscreens/dashboard/P_Dashboard.js
+++ src/provider/Pscreens/dashboard/P_Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import styles from '../../Pcss/dashboard/dashboard';
 import { COLOURS } from '../../../ThemeColour/Theme';
 import * as Animatable from 'react-native-animatable';
