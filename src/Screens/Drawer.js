ea526bd1d0533eaa6f23f8120bcd50218912b077
M-Saffan75 <saffan.khan175@gmail.com>
2024-01-27 14:21:32 -0700

    synta change

--- src/Screens/Drawer.js
+++ src/Screens/Drawer.js
@@ -5,8 +5,8 @@ import React, { useContext, useState, } from 'react'
 import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
