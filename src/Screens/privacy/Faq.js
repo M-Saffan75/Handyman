987edfde199e2341ef14c0092ebc8d3761dec7b9
M-Saffan75 <saffan.khan175@gmail.com>
2024-08-12 12:49:07 -0700

    bracket close

--- src/Screens/privacy/Faq.js
+++ src/Screens/privacy/Faq.js
@@ -12,7 +12,7 @@ import { StatusBar, SafeAreaView, Text, Image, TouchableOpacity, View, ScrollVie
 const Faq = () => {
 
     const navigation = useNavigation();
-    const theme = useContext(themeContext)
+    const theme = useContext(themeContext);
     const [isCollapsed, setIsCollapsed] = useState(true);
     const [isCollapsedvalue, setIsCollapsed_value] = useState(true);
     const [isCollapsedcancel, setIsCollapsed_cancel] = useState(true);
